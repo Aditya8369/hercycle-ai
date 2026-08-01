3a603ed234c988997138778e2478db8f8183d87c
Khushi Singh <khushi897920@gmail.com>
2026-08-01 21:52:00 +0530

    fix(landing): import missing date-utils helpers in page.js and track page.js

--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -30,6 +30,7 @@ import { useOffline } from '@/lib/OfflineContext'
 import { useLocale, useTranslations } from 'next-intl'
 import fetchWithTimeout from '@/lib/fetch-with-timeout'
 import { isEncryptionFailure } from '@/lib/encryption-policy'
+import { getTodayISO, eachDayISO, addDaysISO } from '@/lib/date-utils'
 
 const MONTH_NAMES = ['January','February','March','April','May','June','July','August','September','October','November','December']
 
